295048d44a5561e1beb84cfb0f642ca037eff28e
Ghassan Ajouri <nghassan1998@gmail.com>
2023-10-07 19:14:08 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+const drivers = [
+    { name: 'Ghasan', hometown: 'Irbid' },
+    { name: 'Mohammed', hometown: 'Salt' },
+    { name: 'Ali', hometown: 'Amman' },
+    { name: 'Ammar', hometown: 'London' },
+  ];
+  
+
+  function findMatching(driversArray, query) {
+    return driversArray.filter(driver =>
+      driver.toLowerCase() === query.toLowerCase()
+    );
+  }
+  
+
+  function fuzzyMatch(driversArray, query) {
+    return driversArray.filter(driver =>
+      driver.toLowerCase().startsWith(query.toLowerCase())
+    );
+  }
+
+  function matchName(driverObjects, query) {
+    return driverObjects.filter(driver =>
+      driver.name.toLowerCase() === query.toLowerCase()
+    );
+  }
